3d9e4ff0ea497f88480f540b7a10b21bcfc4dbf6
Daniel Petry <danielpetry@cantab.net>
2019-05-22 22:05:27 +0200

    Change theme back to edited flex

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,106 +1,44 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
-import os
 
 AUTHOR = 'Daniel Petry'
 SITENAME = 'Dan Petry'
-SITEDESCRIPTION = 'Dan Petry\'s website'
-SITEURL = 'http://localhost:8082'
+SITEURL = ''
 
+PATH = 'content'
+
+TIMEZONE = 'Europe/Berlin'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-DEFAULT_PAGINATION = False
-
-# plugins
-PLUGIN_PATHS = ['../pelican-plugins']
-PLUGINS = ['i18n_subsites', 'tipue_search']
-JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
-
-# theme and theme localization
-THEME = './my-theme'
-I18N_GETTEXT_LOCALEDIR = '../pelican-fh5co-marble/locale/'
-I18N_GETTEXT_DOMAIN = 'messages'
-I18N_GETTEXT_NEWSTYLE = True
-TIMEZONE = 'Europe/Berlin'
-DEFAULT_DATE_FORMAT = '%a, %d %b %Y'
-I18N_TEMPLATES_LANG = 'en_US'
-DEFAULT_LANG = 'en'
-LOCALE = 'en_US'
-
-# content paths
-PATH = 'content'
-PAGE_PATHS = ['pages/en']
-ARTICLE_PATHS = ['blog/en']
-
-# i18n
-I18N_SUBSITES = {
-  'de': {
-    'PAGE_PATHS': ['pages/de'],
-    'ARTICLE_PATHS': ['blog/de'],
-    'LOCALE': 'de_DE'
-  }
-}
-
-# logo path, needs to be stored in PATH Setting
-LOGO = '/images/logo.svg'
-
-# special content
-HERO = [
-  {
-    'image': '/images/hero/background-1.jpg',
-    # for multilanguage support, create a simple dict
-    'links': [
-      {
-        'icon': 'icon-code',
-        'url': 'https://github.com/claudio-walser/pelican-fh5co-marble',
-        'text': 'Github'
-      }
-    ]
-  }
-]
+# Blogroll
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-
 SOCIAL = (
     ('github', 'https://github.com/danpetry'),
-    ('residentadvisor', 'https://www.residentadvisor.net/profile/danielpetry/contrib'),
+    ('resident-advisor',
+        'https://www.residentadvisor.net/profile/danielpetry/contrib'),
     ('soundcloud', 'https://soundcloud.com/danielpetry'),
 )
-ABOUT = {
-  'image': '/images/about/about.jpeg',
-  'mail': 'info@gitcd.io',
-  # keep it a string if you dont need multiple languages
-  'text': {
-    'en': 'Learn more about the creator of this theme or just drop a message.',
-    'de': 'Lernen Sie den Author kennen oder hinterlassen Sie einfach eine Nachricht'
-  },
-  'link': 'contact.html',
-  # the address is also taken for google maps
-  'address': 'Zürich, Schweiz',
-  'phone': '+555-shoe'
-}
 
-# navigation and homepage options
-DISPLAY_PAGES_ON_MENU = True
-DISPLAY_PAGES_ON_HOME = True
-DISPLAY_CATEGORIES_ON_MENU = False
-DISPLAY_TAGS_ON_MENU = False
-USE_FOLDER_AS_CATEGORY = True
-PAGE_ORDER_BY = 'order'
+MENUITEMS = (('Production', '/production.html'),
+             ('Coding', '/coding.html'),)
 
-MENUITEMS = [
-  ('Archive', 'archives.html'),
-  ('Contact', 'contact.html')
-]
+DEFAULT_PAGINATION = False
 
-# setup disqus
-DISQUS_SHORTNAME = 'gitcd-dev'
-DISQUS_ON_PAGES = False # if true its just displayed on every static page, like this you can still enable it per page
-
-# setup google maps
-GOOGLE_MAPS_KEY = 'AIzaSyCefOgb1ZWqYtj7raVSmN4PL2WkTrc-KyA'
+USE_LESS = True
+THEME = './my-theme'
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
